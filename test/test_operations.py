74211affb1f13bc6b18852b8549a26728a3a9c61
Karnati S Chandra <Karnati.Chandra@in.ey.com>
2026-01-10 00:35:49 +0530

    Unit test case updated

---
+++ test/test_operations.py
@@ -0,0 +1,11 @@
+from src.mathoperation import add,sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(5,2)==7
+
+def test_sub():
+    assert sub(5,3)==2
+    assert sub(8,3)==5
+    assert sub(3,3) ==0
+    
